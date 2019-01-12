1eb00e94b8af94f9ae5ff4e08b60d43d98dda0cd
eric.bolboa <eric.bolboa@verafin.com>
2019-01-11 22:33:27 -0500

    attempt at Softmax approach

--- Softmax.py
+++ Softmax.py
@@ -7,9 +7,10 @@ def main():
     # bandit probabilities
     bandit_probs = [0.10, 0.50, 0.60, 0.80, 0.10, 
                         0.25, 0.60, 0.45, 0.75, 0.65]
-    
+
     N_experiments = 100  # number of experiments to perform
     N_episodes = 100  # number of episodes per experiment
+    alpha = 0.1  # learning rate
 
 
     class Bandit:
@@ -25,18 +26,43 @@ def main():
             reward = 1 if (rand < self.prob[action]) else 0
             return reward
 
+
     class Agent:
 
-        def __init__(self, bandit):
+        def __init__(self, bandit, alpha):
             self.k = np.zeros(bandit.N, dtype=np.int)  # number of times action was chosen
             self.Q = np.zeros(bandit.N, dtype=np.float)  # estimated value
+            self.mean_reward = 0
+            self.n = 1  # step count
             self.prob_action = None
+            self.alpha = alpha  # learning rate
 
-        def update_Q(self, action, reward):
-            self.k[action] += 1  # increment the number of times an action is chosen
-            # get probability distribution
+        def softmax(self):
             self.prob_action = np.exp(self.Q - np.max(self.Q)) / np.sum(np.exp(self.Q - np.max(self.Q)), axis=0)
-            # update Q action-value using
-            # Q(a) <- Q(a) + 1/(k+1) * (r(a) - Q(a)) * (1 - PR(a))
-            self.Q[action] += (1./self.k[action]) * (reward - self.Q[action]) * (1 - self.prob_action[action])
+
+        def update_Q(self, action, actions_not_taken, reward):
+            self.k[action] += 1  # increment the number of times an action is chosen
+
+            self.mean_reward += (reward - self.mean_reward) / self.n  # mean reward
+
+            # update Q action-value for all actions using
+            # Q(a) <- Q(a) + 1/(k+1) * (r(a) - R) * (1 - PR(a))
+            self.Q[action] += self.alpha * (reward - self.mean_reward) * (1 - self.prob_action[action])
+            # update actions not taken
+            self.Q[actions_not_taken] += self.alpha * (reward - self.mean_reward) * (1 - self.prob_action[actions_not_taken])
+        
+        def get_action(self):
+            self.softmax()  # get probability distribution
+            return np.random.choice(self.Q, p=self.prob_action)  # choose random action
+
+    def experiment(agent, bandit):
+        for episode in range(N_episodes):
+            action = agent.get_action(bandit, alpha)
+            actions_not_taken = agent.Q != action
+            reward = bandit.get_reward(bandit_probs)
+            agent.update_Q(action, actions_not_taken, reward)
+            
+            
+
+main()
 
