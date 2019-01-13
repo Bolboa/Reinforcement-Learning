Fixed the softmax approach

MAB.py
@@ -8,8 +8,8 @@ def main():
     bandit_probs = [0.10, 0.50, 0.60, 0.80, 0.10, 
                         0.25, 0.60, 0.45, 0.75, 0.65]
     
-    N_experiments = 600  # number of experiments to perform
-    N_episodes = 400  # number of episodes per experiment
+    N_experiments = 100  # number of experiments to perform
+    N_episodes = 300  # number of episodes per experiment
     epsilon = 0.1  # probability of random exploration
 
     class Bandit:
@@ -62,34 +62,35 @@ def main():
         return (np.array(action_history), np.array(reward_history))
 
     N_bandits = len(bandit_probs)
-    print("Running multi-armed bandits with N_bandits = {} and agent epsilon = {}".format(N_bandits, epsilon))
-    avg_reward_history = []
-    # increase training after every set of experiment
-    for ep in range(1, N_episodes):
-        reward_history_avg = np.zeros(ep)  # averaged over every episode
-        action_history_sum = ((ep, N_bandits))  # sum action history
-        for i in range(N_experiments):
-            bandit = Bandit(bandit_probs)  # initialize bandits
-            agent = Agent(bandit, epsilon)  # initialize agent
-            (action_history, reward_history) = experiment(agent, bandit, ep)  # perform experiment
+    # print("Running multi-armed bandits with N_bandits = {} and agent epsilon = {}".format(N_bandits, epsilon))
+    # avg_reward_history = []
+    # # increase training after every set of experiment
+    # for ep in range(1, N_episodes):
+    reward_history_avg = np.zeros(N_episodes)  # averaged over every eN_episodessode
+    action_history_sum = ((N_episodes, N_bandits))  # sum action history
+    for i in range(N_experiments):
+        bandit = Bandit(bandit_probs)  # initialize bandits
+        agent = Agent(bandit, epsilon)  # initialize agent
+        (action_history, reward_history) = experiment(agent, bandit, N_episodes)  # perform experiment
 
-            # sum up rewards for all experiments
-            reward_history_avg += reward_history
-            print(reward_history_avg)
+        # sum up rewards for all experiments
+        reward_history_avg += reward_history
+        print(reward_history_avg)
 
-        reward_history_avg /= np.float(N_experiments)
-        avg_reward_history.append(float(np.sum(reward_history_avg)) / float(len(reward_history_avg)))
-        print("reward history avg = {}".format(reward_history_avg))
+    reward_history_avg /= np.float(N_experiments)
+    print("reward history avg = {}".format(reward_history_avg))
+    # avg_reward_history.append(float(np.sum(reward_history_avg)) / float(len(reward_history_avg)))
+    # print("reward history avg = {}".format(reward_history_avg))
 
-    print(avg_reward_history)
-    print(len(avg_reward_history))
+    # print(avg_reward_history)
+    # print(len(avg_reward_history))
 
-    with open("incremental_result.txt", "w") as f:
-        for item in avg_reward_history:
-            f.write("%s\n" % item)
+    # with open("incremental_result.txt", "w") as f:
+    #     for item in avg_reward_history:
+    #         f.write("%s\n" % item)
 
-    plt.plot(avg_reward_history)
-    plt.ylabel("Avg Reward")
-    plt.show()
+    # plt.plot(avg_reward_history)
+    # plt.ylabel("Avg Reward")
+    # plt.show()
 
 main()

Softmax.py
@@ -3,22 +3,18 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    
-    # bandit probabilities
+
     bandit_probs = [0.10, 0.50, 0.60, 0.80, 0.10, 
                         0.25, 0.60, 0.45, 0.75, 0.65]
 
-    N_experiments = 100  # number of experiments to perform
+    N_experiments = 1000  # number of experiments to perform
     N_episodes = 100  # number of episodes per experiment
-    alpha = 0.1  # learning rate
-
 
     class Bandit:
-
         def __init__(self, bandit_probs):
             self.N = len(bandit_probs)  # number of bandits
             self.prob = bandit_probs
-
+        
         # Get reward (1 for success or 0)
         def get_reward(self, action):
             rand = np.random.random()
@@ -26,57 +22,64 @@ def main():
             reward = 1 if (rand < self.prob[action]) else 0
             return reward
 
-
     class Agent:
-
-        def __init__(self, bandit, alpha):
-            self.k = np.zeros(bandit.N, dtype=np.int)  # number of times action was chosen
-            self.Q = np.zeros(bandit.N, dtype=np.float)  # estimated value
+    
+        def __init__(self, bandit):
+            # Number of arms
+            self.k = np.zeros(bandit.N, dtype=np.int)
+            # Step count
+            self.n = 1
+            # Total mean reward
             self.mean_reward = 0
-            self.n = 1  # step count
-            self.prob_action = None
-            self.alpha = alpha  # learning rate
-
+            # Initialize preferences
+            self.Q = np.zeros(bandit.N, dtype=np.float)
+                
         def softmax(self):
             self.prob_action = np.exp(self.Q - np.max(self.Q)) / np.sum(np.exp(self.Q - np.max(self.Q)), axis=0)
-
+            
         def update_Q(self, action, actions_not_taken, reward):
-            self.k[action] += 1  # increment the number of times an action is chosen
+            # Update probabilities
+            self.softmax()
+            
+            # Update counts
+            self.n += 1
+            self.k[action] += 1
+            
+            # Update total
+            self.mean_reward = self.mean_reward + (
+                reward - self.mean_reward) / self.n
+            
+            # Update preferences
+            self.Q[action] = self.Q[action] + \
+                (1./self.k[action]) * (reward - self.mean_reward) * (1 - self.prob_action[action])
 
-            self.mean_reward += (reward - self.mean_reward) / self.n  # mean reward
+            self.Q[actions_not_taken] = self.Q[actions_not_taken] - \
+                (1./self.k[action]) * (reward - self.mean_reward) * self.prob_action[actions_not_taken]
 
-            # update Q action-value for all actions using
-            # Q(a) <- Q(a) + 1/(k+1) * (r(a) - R) * (1 - PR(a))
-            self.Q[action] += self.alpha * (reward - self.mean_reward) * (1 - self.prob_action[action])
-            # update actions not taken
-            self.Q[actions_not_taken] += self.alpha * (reward - self.mean_reward) * (1 - self.prob_action[actions_not_taken])
-        
         def get_action(self):
-            self.softmax()  # get probability distribution
-            return np.random.choice(self.Q, p=self.prob_action)  # choose random action
-    
+            return np.random.choice(np.flatnonzero(self.Q == self.Q.max()))  # choose random action
+
     def experiment(agent, bandit, N_episodes):
-        action_history = []
         reward_history = []
-        for episode in range(N_episodes):
+        for i in range(N_episodes):
             action = agent.get_action()
             actions_not_taken = agent.Q != action
-            reward = bandit.get_reward(int(action))
-            agent.update_Q(int(action), actions_not_taken, reward)
-            action_history.append(action)
-            reward_history.append(reward)
-        return (np.array(action_history), np.array(reward_history))
-
-    N_bandits = len(bandit_probs)
+            reward = bandit.get_reward(action)
+            agent.update_Q(action, actions_not_taken, reward)
+            reward_history.append(agent.mean_reward)
+        return reward_history
+            
     reward_history_avg = np.zeros(N_episodes)
-    action_history_sum = ((N_episodes, N_bandits))  # sum action history
-    for i in range(N_experiments):
+
+    # Run experiments
+    for i in range(N_experiments):        
         bandit = Bandit(bandit_probs)
-        agent = Agent(bandit, alpha)
-        (action_history, reward_history) = experiment(agent, bandit, N_episodes)  # perform experiment
-        reward_history_avg += reward_history
+        agent = Agent(bandit)
+        reward_history = experiment(agent, bandit, N_episodes)
+        
+        # Update long-term averages
+        reward_history_avg += (reward_history - reward_history_avg) / (i + 1)
+
         print(reward_history_avg)
-    reward_history_avg /= np.float(N_experiments)
-    print("reward history avg = {}".format(reward_history_avg))
 
 main()